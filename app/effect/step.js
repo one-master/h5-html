4abad12ce9c4c10bbe4ff33db0c74b694ad46bf2
richardgong <909253305@qq.com>
2015-09-26 16:04:43 +0800

    submit

--- app/effect/step.js
+++ app/effect/step.js
@@ -1,98 +1,128 @@
-var PageTransitions = (function() {
+$(function(){
+    var PageTransitions = (function () {
 
-	var $main = $( '#pt-main' ),
-		$pages = $main.children( 'div.pt-page' ),
-		$iterate = $( '#iterateEffects' ),
-		animcursor = 1,
-		pagesCount = $pages.length,
-		current = 0,
-		isAnimating = false,
-		endCurrPage = false,
-		endNextPage = false,
-		animEndEventNames = {
-			'WebkitAnimation' : 'webkitAnimationEnd',
-			'OAnimation' : 'oAnimationEnd',
-			'msAnimation' : 'MSAnimationEnd',
-			'animation' : 'animationend'
-		},
-		// animation end event name
-		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
-		// support css animations
-		support = Modernizr.cssanimations;
-	
-	function init() {
+        var $main = $('#pt-main'),
+            $pages = $main.children('div.pt-page'),
+            $iterate = $('#iterateEffects'),
+            animcursor = 1,
+            pagesCount = $pages.length,
+            current = 0,
+            isAnimating = false,
+            endCurrPage = false,
+            endNextPage = false,
+            animEndEventNames = {
+                'WebkitAnimation': 'webkitAnimationEnd',
+                'OAnimation': 'oAnimationEnd',
+                'msAnimation': 'MSAnimationEnd',
+                'animation': 'animationend'
+            },
+        // animation end event name
+            animEndEventName = animEndEventNames[ Modernizr.prefixed('animation') ],
+        // support css animations
+            support = Modernizr.cssanimations;
 
-		$pages.each( function() {
-			var $page = $( this );
-			$page.data( 'originalClassList', $page.attr( 'class' ) );
-		} );
+        function init() {
 
-		$pages.eq( current ).addClass( 'pt-page-current' );
-		
-		$('.test').click(function(){
-			nextPage();
-		});
+            $pages.each(function () {
+                var $page = $(this);
+                $page.data('originalClassList', $page.attr('class'));
+            });
+
+            $pages.eq(current).addClass('pt-page-current');
+            var answer = [
+                [false, true],
+                [true, false],
+                [true, false],
+                [false, true],
+                [true, false]
+            ]
+            $(".pt-page-question").each(function(i){
+                var dd= $(this).find('dd');
+                dd.on('click',answer[i],function(e){
+                    dd.find('.selectdot').show();
+                    dd.find(".check").hide();
+                    var curIndex = $(this).index();
+                    $(this).find('.selectdot').hide().siblings().show();
+
+                    if(   (e.data[curIndex]))  {
+                        $('.yes').show();
+                        nextPage();
+
+                    }else{
+                        $('.yes img').attr('src','assets/images/no.gif')
+
+                    };
+
+                });
+
+            })
+            $('.test').click(function () {
+                nextPage();
+            });
 
 
-	}
+        }
 
-	function nextPage( animation ) {
-		if( isAnimating ) {
-			return false;
-		}
+        function nextPage(animation) {
+            if (isAnimating) {
+                return false;
+            }
 
-		isAnimating = true;
-		
-		var $currPage = $pages.eq( current );
+            isAnimating = true;
 
-		if( current < pagesCount - 1 ) {
-			++current;
-		}
-		else {
-			current = 0;
-		}
+            var $currPage = $pages.eq(current);
 
-		var $nextPage = $pages.eq( current ).addClass( 'pt-page-current' ),
-				outClass = 'pt-page-scaleDown';
-				inClass = 'pt-page-moveFromBottom pt-page-ontop';
+            if (current < pagesCount - 1) {
+                ++current;
+            }
+            else {
+                current = 0;
+            }
+
+            var $nextPage = $pages.eq(current).addClass('pt-page-current'),
+                outClass = 'pt-page-scaleDown';
+            inClass = 'pt-page-moveFromBottom pt-page-ontop';
 
 
-		$currPage.addClass( outClass ).on( animEndEventName, function() {
-			$currPage.off( animEndEventName );
-			endCurrPage = true;
-			if( endNextPage ) {
-				onEndAnimation( $currPage, $nextPage );
-			}
-		} );
+            $currPage.addClass(outClass).on(animEndEventName, function () {
+                $currPage.off(animEndEventName);
+                endCurrPage = true;
+                if (endNextPage) {
+                    onEndAnimation($currPage, $nextPage);
+                }
+            });
 
-		$nextPage.addClass( inClass ).on( animEndEventName, function() {
-			$nextPage.off( animEndEventName );
-			endNextPage = true;
-			if( endCurrPage ) {
-				onEndAnimation( $currPage, $nextPage );
-			}
-		} );
+            $nextPage.addClass(inClass).on(animEndEventName, function () {
+                $nextPage.off(animEndEventName);
+                endNextPage = true;
+                if (endCurrPage) {
+                    onEndAnimation($currPage, $nextPage);
+                }
+            });
 
-		if( !support ) {
-			onEndAnimation( $currPage, $nextPage );
-		}
+            if (!support) {
+                onEndAnimation($currPage, $nextPage);
+            }
 
-	}
+        }
 
-	function onEndAnimation( $outpage, $inpage ) {
-		endCurrPage = false;
-		endNextPage = false;
-		resetPage( $outpage, $inpage );
-		isAnimating = false;
-	}
+        function onEndAnimation($outpage, $inpage) {
+            endCurrPage = false;
+            endNextPage = false;
+            resetPage($outpage, $inpage);
+            isAnimating = false;
+        }
 
-	function resetPage( $outpage, $inpage ) {
-		$outpage.attr( 'class', $outpage.data( 'originalClassList' ) );
-		$inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current' );
-	}
+        function resetPage($outpage, $inpage) {
+            $outpage.attr('class', $outpage.data('originalClassList'));
+            $inpage.attr('class', $inpage.data('originalClassList') + ' pt-page-current');
+        }
 
-	init();
+        init();
 
-	return { init : init };
+        return { init: init };
 
-})();
+    })();
+
+
+})

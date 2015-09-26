6e6c9c2ff3a6c4e0813f8aae2185a769878ebd16
richardgong <richardgong@richardgongs-MacBook-Pro.local>
2015-09-27 00:30:58 +0800

    submit

--- app/effect/step.js
+++ app/effect/step.js
@@ -36,7 +36,9 @@ $(function(){
                 [false, true],
                 [true, false]
             ];
+
             var isCorrect = true;
+
             $(".pt-page-question").each(function(i){
                 var dd= $(this).find('.answer-right dd');
                 dd.on('click',answer[i],function(e){
@@ -76,6 +78,48 @@ $(function(){
             });
 
 
+            //touch   event --------------------------- start
+            var startX,
+                startY,
+                $this = $('body');
+
+            $this.bind('touchstart', touchstart);
+
+            function touchstart(event) {
+                var touches = event.originalEvent.touches;
+                if (touches && touches.length) {
+                    startX = touches[0].pageX;
+                    startY = touches[0].pageY;
+                    $this.bind('touchmove', touchmove);
+                }
+            }
+
+            function touchmove(event) {
+                var touches = event.originalEvent.touches;
+                if (touches && touches.length) {
+                    var deltaX = startX - touches[0].pageX;
+                    var deltaY = startY - touches[0].pageY;
+
+                    if (deltaX >= 50) {
+                        $this.trigger("swipeLeft");
+                    }
+                    if (deltaX <= -50) {
+                        $this.trigger("swipeRight");
+                    }
+                    if (deltaY >= 50) {
+                        $this.trigger("swipeUp");
+                    }
+                    if (deltaY <= -50) {
+                        $this.trigger("swipeDown");
+                    }
+                    if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
+                        $this.unbind('touchmove', touchmove);
+                    }
+                }
+            }
+            //touch   event --------------------------- end
+
+
         }
 
         function nextPage(animation) {
@@ -90,9 +134,7 @@ $(function(){
             if (current < pagesCount - 1) {
                 ++current;
             }
-            else {
-                current = 0;
-            }
+
             var dongClass=['pt-page-moveFromTop','pt-page-rotateCarouselRightIn','pt-page-rotateCarouselLeftIn','pt-page-scaleUp','pt-page-scaleUpDown']
             var $nextPage = $pages.eq(current).addClass('pt-page-current'),
                 outClass = 'pt-page-scaleDown';

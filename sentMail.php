6d35b43e7dc0c46b1a11eeff4a7a4539b1839bee
richardgong <richardgong@richardgongs-MacBook-Pro.local>
2015-09-26 23:29:47 +0800

    submit

--- sentMail.php
+++ sentMail.php
@@ -2,15 +2,8 @@
 
 $to = "feminagame@163.com"; //收件人  
 $subject = "答提有奖"; //主题  
-
-
 $message = "用户名:".$_GET['user']."手机号:".$_GET['phone']; //正文  
-
-
-
-var $result = mail($to,$subject,$message);  
-echo $result;
-
+echo mail($to,$subject,$message);  
 ?>
 
 

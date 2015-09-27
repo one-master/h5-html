70b0b1c4f8f7ac106efcfc1479836de8866766b2
richardgong <richardgong@richardgongs-MacBook-Pro.local>
2015-09-27 08:54:33 +0800

    submit

--- app/effect/step.js
+++ app/effect/step.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     var PageTransitions = (function () {
 
         var $main = $('#pt-main'),
@@ -10,6 +10,8 @@ $(function(){
             isAnimating = false,
             endCurrPage = false,
             endNextPage = false,
+            stopAutoMove = false,
+            hasIncorrect = false,
             animEndEventNames = {
                 'WebkitAnimation': 'webkitAnimationEnd',
                 'OAnimation': 'oAnimationEnd',
@@ -17,7 +19,7 @@ $(function(){
                 'animation': 'animationend'
             },
         // animation end event name
-            animEndEventName = animEndEventNames[ Modernizr.prefixed('animation') ],
+            animEndEventName = animEndEventNames[Modernizr.prefixed('animation')],
         // support css animations
             support = Modernizr.cssanimations;
 
@@ -30,42 +32,45 @@ $(function(){
 
             $pages.eq(current).addClass('pt-page-current');
             var answer = [
-                [false, true],
                 [true, false],
                 [true, false],
-                [false, true],
+                [true, false],
+                [true, false],
                 [true, false]
             ];
 
             var isCorrect = true;
 
-            $(".pt-page-question").each(function(i){
-                var dd= $(this).find('.answer-right dd');
-                dd.on('click',answer[i],function(e){
+            $(".pt-page-question").each(function (i) {
+                var dd = $(this).find('.answer-right dd');
+                dd.on('click', answer[i], function (e) {
                     dd.find('.selectdot').show();
                     dd.find(".check").hide();
-                    var curIndex = $(this).index()-1;
+                    var curIndex = $(this).index() - 1;
                     $(this).find('.selectdot').hide().siblings().show();
 
-                    if((e.data[curIndex]))  {
+                    if ((e.data[curIndex])) {
                         $('.yes').show();
                         isCorrect = true;
-                        $('.yes img').attr('src','assets/images/yes.gif');
-                        nextPage();
-
-                    }else{
+                        $('.yes img').attr('src', 'assets/images/yes.gif');
+                        setTimeout(function () {
+                            nextPage();
+                        }, 700);
+                    } else {
                         isCorrect = false;
+                        hasIncorrect = true;
                         $(this).parents('.pt-page-question').find(".answer-error").show();
                         $(this).parents('.pt-page-question').find(".answer-right").hide();
 
-                        $('.yes img').attr('src','assets/images/no.gif')
-                    };
+                        $('.yes img').attr('src', 'assets/images/no.gif')
+                    }
+                    ;
 
                 });
 
             });
 
-            $('.reanswer').click(function(){
+            $('.reanswer').click(function () {
                 $('.yes,.check').hide();
                 $('.selectdot').show();
                 $(this).parents('.pt-page-question').find(".answer-error").hide;
@@ -101,28 +106,38 @@ $(function(){
                     var deltaY = startY - touches[0].pageY;
 
                     if (deltaX >= 50) {
-                        $this.trigger("swipeLeft");
+                        //$this.trigger("swipeLeft");
                     }
                     if (deltaX <= -50) {
-                        $this.trigger("swipeRight");
+                        //$this.trigger("swipeRight");
                     }
                     if (deltaY >= 50) {
-                        $this.trigger("swipeUp");
+                        //$this.trigger("swipeUp");
+                        !stopAutoMove && nextPage();
                     }
                     if (deltaY <= -50) {
-                        $this.trigger("swipeDown");
+                        //$this.trigger("swipeDown");
                     }
                     if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
                         $this.unbind('touchmove', touchmove);
                     }
                 }
             }
+
             //touch   event --------------------------- end
 
 
         }
 
-        function nextPage(animation) {
+        function totaoResult($nextPage) {
+            if ($nextPage.hasClass('total-finish') && hasIncorrect) {
+                $('.finish-error').show().siblings().hide();
+            }
+        }
+        function endNextAnimation(){
+            $('.yes').hide();
+        }
+        function nextPage() {
             if (isAnimating) {
                 return false;
             }
@@ -133,23 +148,31 @@ $(function(){
 
             if (current < pagesCount - 1) {
                 ++current;
+            } else {
+                return;
             }
 
-            var dongClass=['pt-page-moveFromTop','pt-page-rotateCarouselRightIn','pt-page-rotateCarouselLeftIn','pt-page-scaleUp','pt-page-scaleUpDown']
-            var $nextPage = $pages.eq(current).addClass('pt-page-current'),
-                outClass = 'pt-page-scaleDown';
-            inClass = 'pt-page-moveFromBottom pt-page-ontop';
+            var dongClass = ['pt-page-moveFromTop', 'pt-page-rotateCarouselRightIn', 'pt-page-rotateCarouselLeftIn', 'pt-page-scaleUp', 'pt-page-scaleUpDown']
+            var $nextPage = $pages.eq(current).addClass('pt-page-current');
+            var outClass = 'pt-page-scaleDown';
+            var inClass = 'pt-page-moveFromBottom pt-page-ontop';
+            var cur = $nextPage.find("[data-aniclss]");
+            if ($nextPage.hasClass('pt-page-question')) {
+                stopAutoMove = true;
+                $('.Move').hide();
+            } else {
+                stopAutoMove = false;
+                $('.Move').show();
+            }
+            totaoResult($nextPage)
             $pages.find("[data-aniclss]").removeClass(dongClass.join(' '));
-            var cur =$nextPage.find("[data-aniclss]");
-
-
-
-
+            endNextAnimation();
             $currPage.addClass(outClass).on(animEndEventName, function () {
                 $currPage.off(animEndEventName);
                 endCurrPage = true;
                 if (endNextPage) {
                     cur.addClass(cur.data('aniclss'));
+
                     onEndAnimation($currPage, $nextPage);
                 }
             });
@@ -169,6 +192,7 @@ $(function(){
         }
 
         function onEndAnimation($outpage, $inpage) {
+
             endCurrPage = false;
             endNextPage = false;
             resetPage($outpage, $inpage);
@@ -182,7 +206,7 @@ $(function(){
 
         init();
 
-        return { init: init };
+        return {init: init};
 
     })();
 
